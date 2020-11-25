19a7312a1b09e102789821e20de89a45d635a2f5
sparkproda <besteffort@naver.com>
2020-11-25 07:47:50 +0000

    6st commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("<H1> Hello Eric super Sparkproda !!!! </H1>");
+    response.end("<H1> !!!! Hello Eric super Sparkproda !!!! </H1>");
 });
 
 const port = process.env.PORT || 3000;
